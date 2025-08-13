32cd173177b1a00ccb7ceadfbb242dd6c87def8f
Spencer <76940991+spencer1202@users.noreply.github.com>
2025-08-13 14:27:05 -0700

    Created topic evaluation functions, added new config file to simulator

---
+++ server/policy/topic/topic.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+//Input:
+//func (top topic) Parse (str string) (topic, error) {
+
+func main() {
+	topics := []string{
+		"A/B/C",
+		"A/B/#",
+		"A/B/Z",
+		"A/+/C",
+		"A/+/#",
+		"A/Y/Z",
+		"X/B/C",
+		"+/B/C",
+		"+/B",
+		"X/Y/Z",
+		"X/#",
+		"+/+/#",
+	}
+
+	//test(topics, "\"%v\" is a subset of \"%v\"\n", "\"%v\" is NOT a subset of \"%v\"\n", IsSubset)
+	//test(topics, "\"%v\" is a superset of \"%v\"\n", "\"%v\" is NOT a superset of \"%v\"\n", IsSuperset)
+	testing(topics)
+}
+
+func testing(topics []string) {
+
+	runTest := func(topics []string, trueReport string, falseReport string, operation func(string, string) (bool, error)) {
+		for _, str1 := range topics {
+			for _, str2 := range topics {
+				if result, _ := operation(str1, str2); result {
+					fmt.Printf(trueReport, str1, str2)
+				} else {
+					fmt.Printf((falseReport), str1, str2)
+				}
+			}
+		}
+	}
+
+	fmt.Printf("\nTesting IsSubset...\n")
+	runTest(topics, "\"%v\" is a subset of \"%v\"\n", "\"%v\" is NOT a subset of \"%v\"\n", IsSubset)
+	fmt.Printf("\nTesting IsSuperset...\n")
+	runTest(topics, "\"%v\" is a superset of \"%v\"\n", "\"%v\" is NOT a superset of \"%v\"\n", IsSuperset)
+}
+
+// Returns true if the topic in op1 is a subset of op2
+func IsSubset(op1 string, op2 string) (bool, error) {
+	//string is empty
+	if len(op1) == 0 || len(op2) == 0 {
+		var err error
+		return false, err
+	}
+	//strings are equal
+	if op1 == op2 {
+		return true, nil
+	}
+
+	return isSubset(strings.TrimSpace(op1), strings.TrimSpace(op2))
+}
+
+// Recursive function
+func isSubset(op1 string, op2 string) (bool, error) {
+	current1, next1, _ := strings.Cut(op1, "/")
+	current2, next2, _ := strings.Cut(op2, "/")
+
+	//Case 1: both strings are empty
+	if len(current1) == 0 && len(current2) == 0 {
+		return true, nil
+	}
+	if current1 != current2 {
+		//Case 2: one of the strings is empty
+		if len(current1) == 0 || len(current2) == 0 {
+			return false, nil
+		}
+		//Case 3: string2 is a # wildcard
+		if current2 == "#" {
+			var err error
+			//string2 has topics after the wildcard
+			if len(next2) > 0 {
+				return false, err
+			}
+			return true, nil
+		}
+		//Case 4: strings aren't equal, and string2 isn't a + wildcard
+		if current2 != "+" {
+			return false, nil
+		}
+	}
+	return isSubset(next1, next2)
+}
+
+func IsSuperset(op1 string, op2 string) (bool, error) {
+	return IsSubset(op2, op1)
+}
